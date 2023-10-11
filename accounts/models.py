9e31b9c8dad7e05b30515a50503ceafcbda059a5
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-11 18:01:39 +0300

    add confg of static and load it in template

--- accounts/models.py
+++ accounts/models.py
@@ -4,13 +4,13 @@ from django.utils.translation import gettext_lazy  as _
 
 class Profile(models.Model):
     user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
-    name =models.CharField(_('الاسم :'),max_length=40)
-    who_iam = models.TextField(_("من انا :"),max_length=250)
-    price = models.IntegerField(_("سعر الكشف : "))
+    name =models.CharField(_('الاسم '),max_length=40)
+    who_iam = models.TextField(_("من انا "),max_length=250)
+    price = models.IntegerField(_("سعر الكشف "))
 
     class Meta:
         # تحديد اسم الكلاس في admin panel 
-        verbose_name = _("Profile")
-        verbose_name_plural = _("Profiles")
+        verbose_name = _("الملف الشخصي")
+        verbose_name_plural = _("ملفات شخصية")
     def __str__(self):
         return self.name
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+#from django.contrib.auth.models import User
+from .models import Profile
 # Create your views here.
-def app(request):
-    return render(request,'user/app.html')
+def doctors_list(request):
+    doctors = Profile.objects.all()
+    context ={
+        'doctors':doctors
+    }
+    return render(request,'user/doctors_list.html',context=context)
