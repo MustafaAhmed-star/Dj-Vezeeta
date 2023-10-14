3d6602730cd3a413ac6a834c60028bcf82ac1813
Mustafa Abdelmgeed <mostafa012155@gmail.com>
2023-10-14 18:13:23 +0300

    using slug(auto add slug and support any language)

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy  as _
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 class Profile(models.Model):
     user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
     name =models.CharField(_('الاسم '),max_length=40)
     who_iam = models.TextField(_("من انا "),max_length=250)
     price = models.IntegerField(_("سعر الكشف "),blank = True,default=1)
     image = models.ImageField(_("الصورة الشخصية"), upload_to= "profile",blank=True )
-
+    slug = models.SlugField(max_length=200 ,blank=True)
     class Meta:
         # تحديد اسم الكلاس في admin panel 
         verbose_name = _("الملف الشخصي")
@@ -18,7 +19,10 @@ class Profile(models.Model):
         return self.name
     
 
-
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name,allow_unicode=True)
+        super(Profile, self).save(*args, **kwargs)
  
 
 @receiver(post_save, sender=User)
