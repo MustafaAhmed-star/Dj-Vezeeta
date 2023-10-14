bf70a62da7ab7cdba46c06e653aac8a76c016999
Mustafa Abdelmgeed <mostafa012155@gmail.com>
2023-10-14 13:14:53 +0300

    using signals and decorater to create a profile after create a user

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy  as _
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 class Profile(models.Model):
     user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
     name =models.CharField(_('الاسم '),max_length=40)
     who_iam = models.TextField(_("من انا "),max_length=250)
-    price = models.IntegerField(_("سعر الكشف "))
+    price = models.IntegerField(_("سعر الكشف "),blank = True,default=1)
     image = models.ImageField(_("الصورة الشخصية"), upload_to= "profile",blank=True,default='static/profile.jpg' )
 
     class Meta:
@@ -15,3 +16,14 @@ class Profile(models.Model):
         verbose_name_plural = _("ملفات شخصية")
     def __str__(self):
         return self.name
+    
+
+
+ 
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    print('instance',instance )
+    if created:
+        profile = Profile.objects.create(user=instance, name=instance.username)
+         
